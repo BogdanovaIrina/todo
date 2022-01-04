6a35d2622ad8e41063355d1bbf88bd99e47b7343
BogdanovaIrina <irina.bogdanova.1990@gmail.com>
2022-01-04 15:50:30 +0300

    Updates

---
+++ precache-manifest.29e8b59ecec546ec8ce17f90149ee09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e89df7653da272f51addd4e18931150",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5cad736a21dc4ee9c8b4",
+    "url": "/todo/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/todo/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cad736a21dc4ee9c8b4",
+    "url": "/todo/static/js/main.27acad49.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
